cfdb502cfb953c3da07482da165869b736ae9694
KomarovaDiana <144255001+KomarovaDi@users.noreply.github.com>
2023-10-04 18:28:29 +0300

    add files info project

---
+++ script.js
@@ -0,0 +1,16 @@
+var num_1 = 5.4;   
+var num = 3;
+console.log("переменная: " + (num_1 + num));
+
+
+var num_2 = 4;
+num_2--;
+console.log("переменная: " + num_2);
+
+var str_1 = Number("24");   
+var str_2 = "3";
+console.log("переменная: " + (str_1 + str_2));
+
+
+console.log("Math" + Math.PI);
+console.log("Math" + Math.max(400, 7, -8, 3,));
